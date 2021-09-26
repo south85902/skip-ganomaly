d7afcd1f4dca0fb2b66a1e0f305ba929aabe5b3b
JiaNan <south85902@gmail.com>
2021-09-26 15:13:16 +0800

    add Unet fewSkipConnection

--- lib/models/skipganomaly.py
+++ lib/models/skipganomaly.py
@@ -359,8 +359,8 @@ class Skipganomaly(BaseModel):
         with torch.no_grad():
             # Load the weights of netg and netd.
             if self.opt.load_weights:
-                #self.load_weights(is_best=True)
-                self.load_weights(is_last=True)
+                self.load_weights(is_best=True)
+                #self.load_weights(is_last=True)
 
             self.opt.phase = 'test'
 
--- run.py
+++ run.py
@@ -1264,7 +1264,7 @@ try:
     cmd['wgan'] = ''
     cmd['extractor_fine_tuned'] = ''
     cmd['no_padding'] = ''
-    train_eft(cmd)
+    #train_eft(cmd)
 
     l_con = '--l_con l2'
     cmd['phase'] = '--phase val'
@@ -1292,7 +1292,7 @@ try:
     cmd['wgan'] = ''
     cmd['extractor_fine_tuned'] = ''
     cmd['no_padding'] = ''
-    train_eft(cmd)
+    #train_eft(cmd)
 
     l_con = '--l_con l2'
     cmd['phase'] = '--phase val'
@@ -1302,60 +1302,60 @@ except:
 
     sent_message('AnomalyDetectionData_newdata_train0.9_Unet_fewSkipConnection error')
 
-try:
-    cmd = {}
-    cmd['dataset'] = '--dataset AnomalyDetectionData_train0.1'
-    cmd['name'] = '--name AnomalyDetectionData_train0.1_Unet_fewSkipConnection'
-    cmd['isize'] = '--isize 128'
-    cmd['niter'] = '--niter 100'
-    cmd['phase'] = '--phase train'
-    cmd['batchsize'] = '--batchsize 64'
-    cmd['dfr'] = ''
-    cmd['netg'] = '--netg Unet_fewSkipConnection'
-    cmd['l_con'] = '--l_con l1'
-    cmd['discriminator'] = ''
-    cmd['ndf'] = ''
-    cmd['ngf'] = ''
-    cmd['ks'] = ''
-    cmd['wgan'] = ''
-    cmd['extractor_fine_tuned'] = ''
-    cmd['no_padding'] = ''
-    train_eft(cmd)
-
-    l_con = '--l_con l2'
-    cmd['phase'] = '--phase val'
-    testAndeval_eft(cmd)
-except:
-    from line_notify import sent_message
-
-    sent_message('AnomalyDetectionData_train0.1_Unet_fewSkipConnection error')
-
-try:
-    cmd = {}
-    cmd['dataset'] = '--dataset AnomalyDetectionData_train0.5'
-    cmd['name'] = '--name AnomalyDetectionData_train0.5_Unet_fewSkipConnection'
-    cmd['isize'] = '--isize 128'
-    cmd['niter'] = '--niter 100'
-    cmd['phase'] = '--phase train'
-    cmd['batchsize'] = '--batchsize 64'
-    cmd['dfr'] = ''
-    cmd['netg'] = '--netg Unet_fewSkipConnection'
-    cmd['l_con'] = '--l_con l1'
-    cmd['discriminator'] = ''
-    cmd['ndf'] = ''
-    cmd['ngf'] = ''
-    cmd['ks'] = ''
-    cmd['wgan'] = ''
-    cmd['extractor_fine_tuned'] = ''
-    cmd['no_padding'] = ''
-    train_eft(cmd)
-
-    l_con = '--l_con l2'
-    cmd['phase'] = '--phase val'
-    testAndeval_eft(cmd)
-except:
-    from line_notify import sent_message
-
-    sent_message('AnomalyDetectionData_train0.5_Unet_fewSkipConnection error')
+# try:
+#     cmd = {}
+#     cmd['dataset'] = '--dataset AnomalyDetectionData_train0.1'
+#     cmd['name'] = '--name AnomalyDetectionData_train0.1_Unet_fewSkipConnection'
+#     cmd['isize'] = '--isize 128'
+#     cmd['niter'] = '--niter 100'
+#     cmd['phase'] = '--phase train'
+#     cmd['batchsize'] = '--batchsize 64'
+#     cmd['dfr'] = ''
+#     cmd['netg'] = '--netg Unet_fewSkipConnection'
+#     cmd['l_con'] = '--l_con l1'
+#     cmd['discriminator'] = ''
+#     cmd['ndf'] = ''
+#     cmd['ngf'] = ''
+#     cmd['ks'] = ''
+#     cmd['wgan'] = ''
+#     cmd['extractor_fine_tuned'] = ''
+#     cmd['no_padding'] = ''
+#     train_eft(cmd)
+#
+#     l_con = '--l_con l2'
+#     cmd['phase'] = '--phase val'
+#     testAndeval_eft(cmd)
+# except:
+#     from line_notify import sent_message
+#
+#     sent_message('AnomalyDetectionData_train0.1_Unet_fewSkipConnection error')
+#
+# try:
+#     cmd = {}
+#     cmd['dataset'] = '--dataset AnomalyDetectionData_train0.5'
+#     cmd['name'] = '--name AnomalyDetectionData_train0.5_Unet_fewSkipConnection'
+#     cmd['isize'] = '--isize 128'
+#     cmd['niter'] = '--niter 100'
+#     cmd['phase'] = '--phase train'
+#     cmd['batchsize'] = '--batchsize 64'
+#     cmd['dfr'] = ''
+#     cmd['netg'] = '--netg Unet_fewSkipConnection'
+#     cmd['l_con'] = '--l_con l1'
+#     cmd['discriminator'] = ''
+#     cmd['ndf'] = ''
+#     cmd['ngf'] = ''
+#     cmd['ks'] = ''
+#     cmd['wgan'] = ''
+#     cmd['extractor_fine_tuned'] = ''
+#     cmd['no_padding'] = ''
+#     train_eft(cmd)
+#
+#     l_con = '--l_con l2'
+#     cmd['phase'] = '--phase val'
+#     testAndeval_eft(cmd)
+# except:
+#     from line_notify import sent_message
+#
+#     sent_message('AnomalyDetectionData_train0.5_Unet_fewSkipConnection error')
 
 print('done')
