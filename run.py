8487999b2564d34798b462243d76d075a56814b0
JiaNan <south85902@gmail.com>
2021-09-27 10:07:32 +0800

    ssiml1

--- run.py
+++ run.py
@@ -1513,7 +1513,7 @@ try:
     cmd['wgan'] = ''
     cmd['extractor_fine_tuned'] = ''
     cmd['no_padding'] = ''
-    train_eft(cmd)
+    #train_eft(cmd)
 
     l_con = '--l_con l2'
     cmd['phase'] = '--phase val'
