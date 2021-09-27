0fb9fc47630e2e2832106b79405b7e5db2a86f2c
JiaNan <south85902@gmail.com>
2021-09-27 13:52:21 +0800

    re run some exe

--- lib/models/skipganomaly.py
+++ lib/models/skipganomaly.py
@@ -361,8 +361,8 @@ class Skipganomaly(BaseModel):
         with torch.no_grad():
             # Load the weights of netg and netd.
             if self.opt.load_weights:
-                self.load_weights(is_best=True)
-                #self.load_weights(is_last=True)
+                #self.load_weights(is_best=True)
+                self.load_weights(is_last=True)
 
             self.opt.phase = 'test'
 
@@ -544,8 +544,8 @@ class Skipganomaly(BaseModel):
         with torch.no_grad():
             # Load the weights of netg and netd.
             if self.opt.load_weights:
-                self.load_weights(is_best=True)
-                #self.load_weights(is_last=True)
+                #self.load_weights(is_best=True)
+                self.load_weights(is_last=True)
 
             self.opt.phase = 'test'
 
