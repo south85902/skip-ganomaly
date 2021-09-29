247fb267da7846d83c17f4d4794f2a17aa3b407a
JiaNan <south85902@gmail.com>
2021-09-29 15:34:28 +0800

    add noSkipConnection with res block

--- lib/data/dataloader.py
+++ lib/data/dataloader.py
@@ -83,12 +83,12 @@ def load_data(opt):
         #                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
 
         if opt.no_padding:
-            # transform = transforms.Compose([transforms.Resize([opt.isize, opt.isize]),
-            #                                 transforms.ToTensor(),
-            #                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
+            transform = transforms.Compose([transforms.Resize([opt.isize, opt.isize]),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
+        elif opt.resize_same:
             transform = transforms.Compose([transforms.Resize([opt.isize, 32]),
                                             SquarePad(),
-                                            #transforms.Resize([opt.isize, opt.isize]),
                                             transforms.CenterCrop(opt.isize),
                                             transforms.ToTensor(),
                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), ])
