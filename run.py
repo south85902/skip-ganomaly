ee29252353529f1896902e7d0bdcd8a5e774ea0b
JiaNan <south85902@gmail.com>
2021-10-04 13:29:26 +0800

    test

--- run.py
+++ run.py
@@ -1873,7 +1873,7 @@ try:
     cmd['resize_same'] = '--resize_same'
     #train_eft(cmd)
 
-    l_con = '--l_con l1'
+    l_con = '--l_con l2'
     cmd['phase'] = '--phase val'
     testAndeval_eft(cmd)
 except:
