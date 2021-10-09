81f394707d879db59c84e19ecdb41c90c6b46d26
JiaNan <south85902@gmail.com>
2021-10-09 22:26:19 +0800

    train with two skip connection

--- run.py
+++ run.py
@@ -2429,6 +2429,34 @@ except:
     from line_notify import sent_message
     sent_message('AnomalyDetectionData_train0.5_vgg_Unet_twoSkipConnection_resize_same_w32 error')
 
+try:
+    cmd = {}
+    cmd['dataset'] = '--dataset AnomalyDetectionData_train0.5_vgg'
+    cmd['name'] = '--name AnomalyDetectionData_train0.5_vgg_DFR_CAE_noDis_resize_same_w32'
+    cmd['isize'] = '--isize 128'
+    cmd['niter'] = '--niter 50'
+    cmd['phase'] = '--phase train'
+    cmd['batchsize'] = '--batchsize 4'
+    cmd['dfr'] = '--DFR'
+    cmd['netg'] = '--netg CAE'
+    cmd['l_con'] = '--l_con l1'
+    cmd['discriminator'] = '--no_discriminator'
+    cmd['ndf'] = ''
+    cmd['ngf'] = ''
+    cmd['ks'] = '--ks 3'
+    cmd['wgan'] = ''
+    cmd['extractor_fine_tuned'] = ''
+    cmd['no_padding'] = ''
+    cmd['resize_same'] = '--resize_same'
+    # train_eft(cmd)
+
+    cmd['l_con'] = '--l_con l2'
+    cmd['phase'] = '--phase val'
+    testAndeval_eft(cmd)
+except:
+    from line_notify import sent_message
+    sent_message('AnomalyDetectionData_train0.5_vgg_DFR_CAE_noDis_resize_same_w32 error')
+
 # try:
 #     cmd = {}
 #     cmd['dataroot'] = '--dataroot ../dataSet/AnomalyDetectionData_2PIN_train0.5_vgg/train_vgg_all_au'
