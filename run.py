f4074d424699050fde05c091ca4d6bc26f10c10a
JiaNan <south85902@gmail.com>
2021-10-09 23:14:19 +0800

    test load last

--- run.py
+++ run.py
@@ -2243,33 +2243,33 @@ def train_vgg19(cmds):
 #     from line_notify import sent_message
 #     sent_message('AnomalyDetectionData_train0.1_vgg_DFR_CAE_noDis_eft_au0.5_resize_same_w32 error')
 
-# try:
-#     cmd = {}
-#     cmd['dataset'] = '--dataset AnomalyDetectionData_train0.1_vgg'
-#     cmd['name'] = '--name AnomalyDetectionData_train0.1_vgg_Unet_fewSkipConnection_resize_same_w32'
-#     cmd['isize'] = '--isize 128'
-#     cmd['niter'] = '--niter 100'
-#     cmd['phase'] = '--phase train'
-#     cmd['batchsize'] = '--batchsize 64'
-#     cmd['dfr'] = ''
-#     cmd['netg'] = '--netg Unet_fewSkipConnection'
-#     cmd['l_con'] = '--l_con l1'
-#     cmd['discriminator'] = ''
-#     cmd['ndf'] = ''
-#     cmd['ngf'] = ''
-#     cmd['ks'] = '--ks 3'
-#     cmd['wgan'] = ''
-#     cmd['extractor_fine_tuned'] = ''
-#     cmd['no_padding'] = ''
-#     cmd['resize_same'] = '--resize_same'
-#     train_eft(cmd)
-#
-#     cmd['l_con'] = '--l_con l2'
-#     cmd['phase'] = '--phase val'
-#     testAndeval_eft(cmd)
-# except:
-#     from line_notify import sent_message
-#     sent_message('AnomalyDetectionData_train0.1_vgg_Unet_fewSkipConnection_resize_same_w32 error')
+try:
+    cmd = {}
+    cmd['dataset'] = '--dataset AnomalyDetectionData_train0.1_vgg'
+    cmd['name'] = '--name AnomalyDetectionData_train0.1_vgg_Unet_fewSkipConnection_resize_same_w32'
+    cmd['isize'] = '--isize 128'
+    cmd['niter'] = '--niter 100'
+    cmd['phase'] = '--phase train'
+    cmd['batchsize'] = '--batchsize 64'
+    cmd['dfr'] = ''
+    cmd['netg'] = '--netg Unet_fewSkipConnection'
+    cmd['l_con'] = '--l_con l1'
+    cmd['discriminator'] = ''
+    cmd['ndf'] = ''
+    cmd['ngf'] = ''
+    cmd['ks'] = '--ks 3'
+    cmd['wgan'] = ''
+    cmd['extractor_fine_tuned'] = ''
+    cmd['no_padding'] = ''
+    cmd['resize_same'] = '--resize_same'
+    #train_eft(cmd)
+
+    cmd['l_con'] = '--l_con l2'
+    cmd['phase'] = '--phase val'
+    testAndeval_eft(cmd)
+except:
+    from line_notify import sent_message
+    sent_message('AnomalyDetectionData_train0.1_vgg_Unet_fewSkipConnection_resize_same_w32 error')
 #
 # try:
 #     cmd = {}
@@ -2299,33 +2299,33 @@ def train_vgg19(cmds):
 #     from line_notify import sent_message
 #     sent_message('AnomalyDetectionData_train0.5_vgg_Unet_noSkipConnection_resize_same_w32 error')
 #
-# try:
-#     cmd = {}
-#     cmd['dataset'] = '--dataset AnomalyDetectionData_train0.5_vgg'
-#     cmd['name'] = '--name AnomalyDetectionData_train0.5_vgg_Unet_fewSkipConnection_resize_same_w32'
-#     cmd['isize'] = '--isize 128'
-#     cmd['niter'] = '--niter 100'
-#     cmd['phase'] = '--phase train'
-#     cmd['batchsize'] = '--batchsize 64'
-#     cmd['dfr'] = ''
-#     cmd['netg'] = '--netg Unet_fewSkipConnection'
-#     cmd['l_con'] = '--l_con l1'
-#     cmd['discriminator'] = ''
-#     cmd['ndf'] = ''
-#     cmd['ngf'] = ''
-#     cmd['ks'] = '--ks 3'
-#     cmd['wgan'] = ''
-#     cmd['extractor_fine_tuned'] = ''
-#     cmd['no_padding'] = ''
-#     cmd['resize_same'] = '--resize_same'
-#     train_eft(cmd)
-#
-#     cmd['l_con'] = '--l_con l2'
-#     cmd['phase'] = '--phase val'
-#     testAndeval_eft(cmd)
-# except:
-#     from line_notify import sent_message
-#     sent_message('AnomalyDetectionData_train0.5_vgg_Unet_fewSkipConnection_resize_same_w32 error')
+try:
+    cmd = {}
+    cmd['dataset'] = '--dataset AnomalyDetectionData_train0.5_vgg'
+    cmd['name'] = '--name AnomalyDetectionData_train0.5_vgg_Unet_fewSkipConnection_resize_same_w32'
+    cmd['isize'] = '--isize 128'
+    cmd['niter'] = '--niter 100'
+    cmd['phase'] = '--phase train'
+    cmd['batchsize'] = '--batchsize 64'
+    cmd['dfr'] = ''
+    cmd['netg'] = '--netg Unet_fewSkipConnection'
+    cmd['l_con'] = '--l_con l1'
+    cmd['discriminator'] = ''
+    cmd['ndf'] = ''
+    cmd['ngf'] = ''
+    cmd['ks'] = '--ks 3'
+    cmd['wgan'] = ''
+    cmd['extractor_fine_tuned'] = ''
+    cmd['no_padding'] = ''
+    cmd['resize_same'] = '--resize_same'
+    #train_eft(cmd)
+
+    cmd['l_con'] = '--l_con l2'
+    cmd['phase'] = '--phase val'
+    testAndeval_eft(cmd)
+except:
+    from line_notify import sent_message
+    sent_message('AnomalyDetectionData_train0.5_vgg_Unet_fewSkipConnection_resize_same_w32 error')
 #
 # try:
 #     cmd = {}
