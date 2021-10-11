56da455669a18308b0fa7b3842ef30b9f159ec67
JiaNan <south85902@gmail.com>
2021-10-11 09:42:04 +0800

    train DFR 0.5

--- run.py
+++ run.py
@@ -2448,7 +2448,7 @@ try:
     cmd['extractor_fine_tuned'] = ''
     cmd['no_padding'] = ''
     cmd['resize_same'] = '--resize_same'
-    train_eft(cmd)
+    # train_eft(cmd)
 
     cmd['l_con'] = '--l_con l2'
     cmd['phase'] = '--phase val'
